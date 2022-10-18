395bdf2afac7dda9b07f5bb6ca4c2892b7c2098e
Yugaraj.B <114376104+yugaGit@users.noreply.github.com>
2022-10-18 23:10:21 +0530

    Add files via upload
    method overriding in java

---
+++ CusionC.java
@@ -0,0 +1,26 @@
+public class CusionC extends WoodenC{
+
+   // String color="Black";
+   
+   int color=5;
+
+    @Override
+     void seating(){
+        System.out.println("Sponge");
+        }
+    @Override
+     void legs(){
+        System.out.println("4");
+        }
+
+    public static void main(String[]ar){
+
+    CusionC C=new CusionC();
+    C.seating();
+    C.legs();
+   
+
+    }
+
+}
+//override  eg
---
+++ WoodenC.java
@@ -0,0 +1,19 @@
+public class WoodenC{
+    String color="yellow";
+
+    //public static void main(String[]ar){
+    
+     
+        void seating(){
+            System.out.println("Wood");
+        }
+        void legs(){
+            System.out.println("Four");
+        }
+
+    }   
+
+  
+
+
+//override eg
